ee4e17019458971480749d101fff1cf86796e138
Christian Javier Cedillo Palacios <christian.cedillo.palacios@outlook.com>
2024-04-20 23:10:56 -0600

    pages demo

---
+++ src/app/AboutUs/page.jsx
@@ -0,0 +1,9 @@
+export default function AboutUs() {
+  return (
+    <main>
+      <div className="h-screen flex justify-center items-center">
+        <h1 className="text-orange text-2xl">About us</h1>
+      </div>
+    </main>
+  );
+}
